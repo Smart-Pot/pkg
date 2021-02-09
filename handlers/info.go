3c1eb61c17218ef3c8128f1cf102dfe75f328404
Ahmetcan ÖZCAN <ahmetcanozcan7@gmail.com>
2021-02-09 16:01:06 +0300

    Add info handler

--- handlers/info.go
+++ handlers/info.go
@@ -1,5 +1,15 @@
 package handlers
 
-func GetInfoHandler(version) {
+import (
+	"net/http"
 
+	"github.com/Smart-Pot/pkg/common"
+)
+
+func GetInfoHandler(serviceName string, version common.Version) http.Handler {
+	var f http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		// Write Information
+	}
+
+	return f
 }
